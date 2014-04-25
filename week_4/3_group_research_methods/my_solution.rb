3a4a0eeeb8b377bea13fb64ed77b61e338ca2881
Celeen Rusk <celeenrusk@gmail.com>
2014-04-25 15:57:24 -0500

    array_finding_method group project

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -6,7 +6,7 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  # Your code here!
+	source.select {|member| member.include? thing_to_find}
 end
 
 def my_hash_finding_method(source, thing_to_find)
