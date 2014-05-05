372a694336caae311d04d76eeecf5adac7e8bf59
Celeen Rusk <celeenrusk@gmail.com>
2014-05-04 23:22:51 -0500

    bakery challenge

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -1,49 +1,90 @@
 # U2.W5: The Bakery Challenge (GPS 2.1)
 
 # Your Names
-# 1)
-# 2)
+# 1) Celeen Rusk
+# 2) Ken Mendonca
 
  # This is the file you should end up editing. 
- 
-def bakery_num(num_of_people, fav_food)
-  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
-  pie_qty = 0
+ def bakery_num(num_of_people, fav_food)
+  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1} # creates a hash of serving sizes
+  #set quantities of items to zero
+  pie_qty = 0 
   cake_qty = 0
   cookie_qty = 0
-  
-  has_fave = false
-
+  #has_fave set to default as false
+  fave_match = false
+#iterate through each key of hash, and when found, change has_fav to true
   my_list.each_key do |k|
-  if k == fav_food
-  has_fave = true
-  fav_food = k
+      if k == fav_food
+          has_fave = true
+          fav_food = k 
+      end
   end
-  end
-  if has_fave == false
-  raise ArgumentError.new("You can't make that food")
-  else
-  fav_food_qty = my_list.values_at(fav_food)[0]
-  if num_of_people % fav_food_qty == 0
-    num_of_food = num_of_people / fav_food_qty
-    return "You need to make #{num_of_food} #{fav_food}(s)."
-  else num_of_people % fav_food_qty != 0
-    while num_of_people > 0
-    if num_of_people / my_list["pie"] > 0
-    pie_qty = num_of_people / my_list["pie"]
-    num_of_people = num_of_people % my_list["pie"]
-    elsif num_of_people / my_list["cake"] > 0
-    cake_qty = num_of_people / my_list["cake"]
-    num_of_people = num_of_people % my_list["cake"]
+#if fav_food isn't in hash, raise error
+    if has_fave == false
+        raise ArgumentError.new("You can't make that food")
     else
-    cookie_qty = num_of_people
-    num_of_people = 0
+        fav_food_qty = my_list.values_at(fav_food)[0] #if fav_found in hash, give value of key
+        if num_of_people % fav_food_qty == 0 # if remainder of num_of_people / fav_food_qty is 0, it returns equal number of food 
+            num_of_food = num_of_people / fav_food_qty
+            return "You need to make #{num_of_food} #{fav_food}(s)."
+    
+        else num_of_people % fav_food_qty != 0 #if remainder of number of people is NOT equal to zero
+            
+            while num_of_people > 0 
+                if num_of_people / my_list["pie"] > 0 #if number of people divided by value at key pie in hash is greater than zero
+                    pie_qty = num_of_people / my_list["pie"] #set pie quantity to number of people divided by value at key pie hash
+                    num_of_people = num_of_people % my_list["pie"] #re-set number of people to remainder of people divuded by value at key pie in hash
+                elsif num_of_people / my_list["cake"] > 0 #do the same thing for cake
+                    cake_qty = num_of_people / my_list["cake"]
+                    num_of_people = num_of_people % my_list["cake"]
+                else
+                    cookie_qty = num_of_people #losers gets a cookie
+                    num_of_people = 0
+                end
+            end
+            return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)." #print quantities needed for fav_foods
+        end
     end
-    end
-    return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
-    end
-  end
 end
+ 
+#refactoring 
+# def bakery_num(num_of_people, fav_food)
+#   my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+#   pie_qty = 0
+#   cake_qty = 0
+#   cookie_qty = 0
+  
+#   fave_match = false
+
+#   my_list.each_key do |k|
+#     if k == fav_food
+#     fave_match = true
+#     end
+#   end
+
+#     if fave_match == false
+#       raise ArgumentError.new("You can't make that food")
+    
+#     else
+#     fav_food_qty = my_list.values_at(fav_food)[0]
+    
+#     if num_of_people % fav_food_qty == 0
+#       num_of_food = num_of_people / fav_food_qty
+#       return "You need to make #{num_of_food} #{fav_food}(s)."
+    
+#     else num_of_people % fav_food_qty != 0
+#        my_list.each_key do |key|
+#                 num_of_people = []
+#                 if num_of_people > my_list[key] > 0
+#                     pie_qty = num_of_people / my_list[key] #set pie quantity to number of people divided by value at key pie hash
+#                     num_of_people << num_of_people % my_list[key]
+#                 end
+#       end
+#       return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+#     end
+# end
+# end
  
 
 #-----------------------------------------------------------------------------------------------------
@@ -55,6 +96,7 @@ p bakery_num(41, "pie") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s
 p bakery_num(24, "cookie") == "You need to make 24 cookie(s)."
 p bakery_num(4, "pie") == "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s)."
 p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie(s)."
+p bakery_num(25, "cake") == "You need to make 0 pie(s), 4 cake(s), and 1 cookie(s)."
 # p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
