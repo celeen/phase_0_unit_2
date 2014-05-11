9568a359bf35cd77889e1aec168a2baa93a9ebaa
Celeen Rusk <celeenrusk@gmail.com>
2014-05-10 22:24:45 -0500

    boggle board!

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -9,10 +9,27 @@
 
 
 # 3. Initial Solution
+
 class BoggleBoard
- 
-  #your code here
- 
+  attr_reader :boggle_board, :num_rows, :num_cols
+  def initialize(boggle_board)
+    @boggle_board = boggle_board
+    @num_rows = boggle_board.length
+    @num_cols = boggle_board[0].length
+  end
+  
+  def create_word(*coords)
+    coords.map { |coord| @boggle_board[coord.first][coord.last] }.join("")
+  end
+  
+  def get_row(row)
+    @boggle_board[row]
+  end
+
+  def get_col(col)
+    @boggle_board.transpose[col]
+  end
+  
 end
  
  
@@ -22,8 +39,17 @@ dice_grid = [["b", "r", "a", "e"],
              ["t", "a", "k", "e"]]
  
 boggle_board = BoggleBoard.new(dice_grid)
- 
 
+# DRIVER TESTS
+def assert
+  raise "Assertion failed!" unless yield
+end
+assert { boggle_board.create_word([1,2], [1,1], [2,1], [3,2]) == "dock" }
+assert { boggle_board.get_row(2) == ["e", "c", "l", "r"] }
+assert { boggle_board.get_col(1) == ["r", "o", "c", "a"] }
+(0...boggle_board.num_rows).to_a.each { |row| puts boggle_board.get_row(row).join }
+(0...boggle_board.num_cols).to_a.each { |col| puts boggle_board.get_col(col).join }
+assert { boggle_board.get_row(3)[2] == "k" }
 
 # 4. Refactored Solution
 
@@ -34,6 +60,8 @@ boggle_board = BoggleBoard.new(dice_grid)
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
+
+
 # implement tests for each of the methods here:
 
 
